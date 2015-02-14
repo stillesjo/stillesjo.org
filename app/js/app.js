17b0c3fb1ecedef113a74b5ba4602d73c0ffdc6c
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-02-14 18:24:36 +0100

    Gonna test them carousel.

--- app/js/app.js
+++ app/js/app.js
@@ -20,19 +20,7 @@ stillInteractiveApp.controller('AboutController', ['$scope', function($scope) {
   { url:'https://se.linkedin.com/in/stillesjo/en', img:'linkedin.png'},
   { url:'https://twitter.com/stillesjo', img:'twitter.png'}
   ];
-  $scope.skills = [
-  { name: "C++", chron: 0, skillLevel: 9, superSkill: true},
-  { name: "Java", chron: 1, skillLevel: 10, superSkill: true },
-  { name: "HTML/CSS", chron: 2, skillLevel: 6, superSkill: true },
-  { name: "Javascript", chron: 3, skillLevel: 6, superSkill: true },
-  { name: "PHP", chron: 4, skillLevel: 4, superSkill: false },
-  { name: "Python", chron: 5, skillLevel: 5, superSkill: false },
-  { name: "Ruby", chron: 6, skillLevel: 8, superSkill: true },
-  { name: "Ruby On Rails", chron: 7, skillLevel: 7, superSkill: true }
-  ];
-  $scope.isSuperSkill = function(skill) {
-    return ["C++","Java","Ruby", "Javascript"].indexIf(skill) >= 0;
-  }
+  $scope.skills= [ "C++", "Java","Python", "Ruby", "HTML/CSS", "Javascript", "Ruby On Rails", "PHP"];
 }]);
 
 
