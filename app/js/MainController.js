1e15e043ee4d74244d14b72944e086a9d9e6ebe5
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-02-14 11:24:47 +0100

    Stuff ongoing

--- app/js/MainController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var MainController = angular.module('MainController', []);
-
-MainController.controller('MainController', ['$scope',
-    function($scope) {
-      console.log("Angular controller is running...")
-    }]);
--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 var stillInteractiveApp = angular.module('stillInteractiveApp', [
-    'ngRoute', 'mgcrea.ngStrap'//, 'ui.bootstrap'
+    'ngRoute', 'mgcrea.ngStrap', 'ngResource'
 ]);
 
 stillInteractiveApp.controller('HeaderController',['$scope', '$location', function($scope, $location) {
@@ -20,6 +20,19 @@ stillInteractiveApp.controller('AboutController', ['$scope', function($scope) {
   { url:'https://se.linkedin.com/in/stillesjo/en', img:'linkedin.png'},
   { url:'https://twitter.com/stillesjo', img:'twitter.png'}
   ];
+  $scope.skills = [
+  { name: "C++", chron: 0, skillLevel: 9, superSkill: true},
+  { name: "Java", chron: 1, skillLevel: 10, superSkill: true },
+  { name: "HTML/CSS", chron: 2, skillLevel: 6, superSkill: true },
+  { name: "Javascript", chron: 3, skillLevel: 6, superSkill: true },
+  { name: "PHP", chron: 4, skillLevel: 4, superSkill: false },
+  { name: "Python", chron: 5, skillLevel: 5, superSkill: false },
+  { name: "Ruby", chron: 6, skillLevel: 8, superSkill: true },
+  { name: "Ruby On Rails", chron: 7, skillLevel: 7, superSkill: true }
+  ];
+  $scope.isSuperSkill = function(skill) {
+    return ["C++","Java","Ruby", "Javascript"].indexIf(skill) >= 0;
+  }
 }]);
 
 
@@ -29,7 +42,7 @@ stillInteractiveApp.config(['$routeProvider',
         when('/shet', {
           templateUrl: 'partials/SHET.html',
         }).when('/',{
-          templateUrl: 'partials/index.html',
+          templateUrl: 'partials/home.html',
         }).when('/about',{
           templateUrl: 'partials/about.html',
         }).otherwise({
