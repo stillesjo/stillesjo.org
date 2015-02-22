bf7778ba55a843394dad719694ec790e675d7e1f
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-02-22 21:57:50 +0100

    Fix dropdowns

--- app/js/header.js
+++ app/js/header.js
@@ -4,7 +4,8 @@ stillInteractiveApp.controller('HeaderController',['$scope', '$location', functi
     return viewLocation == $location.path();
   };
   $scope.products = [
-  {name:'SHET', href:'shet'}
+  {name:'SHET', href:'shet'},
+  {name:'Experiplan', href:'experiplan'}
   ];
   $scope.hidestuff = function() {
     $("#navbar").collapse("hide");
