e6f352a8baa56c9e64cf741e28a256684e0259bc
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-02-24 22:37:53 +0100

    Fix input validation

--- app/js/app.js
+++ app/js/app.js
@@ -24,9 +24,9 @@ stillInteractiveApp.controller("FooterController", ["$scope", function($scope) {
   }
   $scope.first = Math.floor(Math.random()*10 + 1);
   $scope.second = Math.floor(Math.random()*10 + 1);
+  $scope.result = $scope.first + $scope.second;
 
   $scope.submit = function() {
-    
     $("#still-collapsed-form-collapse").collapse("hide");
     $("#still-success").collapse("show").delay(5000).hide("slow");
   }
